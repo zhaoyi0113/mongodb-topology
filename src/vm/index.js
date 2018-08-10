bde26ef925cf8eb67e5a069d2b3ba86e22166286
Joey Zhao <zhaoyi0113@gmail.com>
2018-08-10 15:28:15 +1000

    add vm class

---
+++ src/vm/index.js
@@ -0,0 +1,29 @@
+const { NodeVM } = require("vm2");
+
+class DBSandBox {
+  constructor(driver) {
+    this.driver = driver;
+  }
+
+  runNativeCode(db, code) {
+    let log = "";
+    const l = e => {
+      log += e;
+    };
+    const vm = new NodeVM({
+      timeout: 1000,
+      sandbox: { db },
+      console: "redirect"
+    });
+    vm.on("console.log", l);
+    vm.run(code);
+    return log;
+  }
+
+  runNativeDBCode(dbName, code) {
+    const db = this.driver.db(dbName);
+    this.runNativeCode(db, code);
+  }
+}
+
+module.exports = DBSandBox;
